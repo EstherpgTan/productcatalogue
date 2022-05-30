407799679a8d213564e87718d6dc2f29d0e5590a
Esther Tan <EstherpgTan@gmail.com>
2022-05-30 23:09:50 +1000

    Bug fixes, rearranging of methods, new Catalogue class added

--- Program.cs
+++ Program.cs
@@ -14,30 +14,3 @@ namespace productcatalogue
     }
 }
 
-// new class menu
-// display menu method
-// empty constructor
-// display  menu take in an array of options. Render each option to the console.
-
-//View a catalogue
-//Add a new product to a catalogue
-//Remove a product
-//Remove a catalogue
-
-// Inside program create a new instance of menu class, run display method on instance
-// .eg. menu.DisplayOptions pass in array of view of catalogue etc
-// Create a new instance of a catalogue
-
-// Console.ReadLine()
-// Switch statement
-// Use the printCollection method to print
-// If add product to catalgue : Create a new bag or shoe ? Run the new bag/shoe method
-
-// Remove a product
-// What product you want to remove ?
-// FInd product in catalogue list _Collection
-// Then remove it
-
-
-// Draw a diagram of e.g. remove a product
-// Do the menu
